50cf3a8d9d7982b29a1ce48a09a5c52fdec5ffe8
luksi1 <luke.simmons@gmail.com>
2018-05-14 14:22:31 +0200

    fixed find error + added formatting via shfmt for bash scripts

---
+++ target/generated-sources/groovy-stubs/test/DatabaseConnectionIT.java
@@ -0,0 +1,56 @@
+//
+// Generated stub from file:/root/docker-mssql/src/test/groovy/se/lukesimmons/mssql/DatabaseConnectionIT.groovy
+//
+
+// Default package
+
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.sql.*;
+import java.util.Enumeration;
+import java.io.*;
+import junit.framework.Test;
+import junit.textui.TestRunner;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.matchers.JUnitMatchers.*;
+
+public class DatabaseConnectionIT
+    extends GroovyTestCase
+    implements groovy.lang.GroovyObject
+{
+    public void testConnection() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void testConnectionFailsWithWrongPassword() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public groovy.lang.MetaClass getMetaClass() {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void setMetaClass(groovy.lang.MetaClass metaClass) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.Object invokeMethod(java.lang.String name, java.lang.Object args) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public java.lang.Object getProperty(java.lang.String name) {
+        throw new InternalError("Stubbed method");
+    }
+
+    public void setProperty(java.lang.String name, java.lang.Object value) {
+        throw new InternalError("Stubbed method");
+    }
+}
